9e823afd544a347e64e550df8b572a1265aadf0b
pszemek <przemyslaw.miekos@gmail.com>
2025-03-12 06:38:45 +0100

    update-images.sh script changed
    User can pick a folder(service) to build. If nothing is picked, all services will be built.

--- update-images.sh
+++ update-images.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
+# Set up Docker environment for Minikube
 eval $(minikube -p minikube docker-env)
+
 # Get the current directory
 BASE_DIR=$(pwd)
 
-# Loop through each subdirectory (1 level deep)
-for dir in */ ; do
-    # Remove the trailing slash to get the folder name
-    FOLDER_NAME=$(basename "$dir")
+# Check if an argument was provided
+if [ -n "$1" ]; then
+    IFS=',' read -r -a SERVICES <<< "$1"
+else
+    # If no argument is provided, get all subdirectories
+    SERVICES=($(find . -maxdepth 1 -type d -not -path '.' -exec basename {} \;))
+fi
 
-    # Check if it's a directory
-    if [ -d "$dir" ]; then
+# Loop through the specified services
+for FOLDER_NAME in "${SERVICES[@]}"; do
+    if [ -d "$FOLDER_NAME" ]; then
         echo "Processing $FOLDER_NAME..."
 
         # Change to the directory
         cd "$BASE_DIR/$FOLDER_NAME" || exit
         IMAGE_NAME=localhost:5000/betting-app/$FOLDER_NAME
+
         # Execute Maven command
         mvn clean spring-boot:build-image -Dspring-boot.build-image.imageName="$IMAGE_NAME"
         docker push "$IMAGE_NAME"
 
         # Return to the base directory
         cd "$BASE_DIR" || exit
+    else
+        echo "Skipping $FOLDER_NAME: Directory not found."
     fi
 done
